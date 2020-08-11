7eaf124fe2925273b4a4e6eb587d64ec54a41237
toby <toby.bell1702@hotmail.co.uk>
2020-08-11 18:17:42 +0100

    fixed permission bug

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
 console.log("Background running");
-chrome.browserAction.onClicked.addListener(openNewTab)
+chrome.browserAction.onClicked.addListener(takeScreenshot)
 
- function openNewTab(tab) {
-   chrome.tabs.create({'url':"chrome://newtab"})
-       chrome.tabs.captureVisibleTab(null,{},function(dataUri){
-           console.log(dataUri);
-       });
-     }
+function takeScreenshot(tab) {
+  chrome.tabs.captureVisibleTab(null,{},function(dataUri){
+  console.log(dataUri);
+  openNewTab();
+});
+
+}
+function openNewTab() {
+  chrome.tabs.create({'url':"chrome://newtab"})
+}
