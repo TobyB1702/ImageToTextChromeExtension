0caff9156fc5b3471b6640c2af38138ee9b0ed7b
toby <toby.bell1702@hotmail.co.uk>
2020-08-14 16:59:09 +0100

    Reworked so screenshot is now saved

--- background.js
+++ background.js
@@ -1,24 +1,59 @@
-console.log("Background running");
-
 var screenshot = {
+    content : document.createElement("canvas"),
     data : '',
+
     init : function() {
-    this.initEvents();
+        this.initEvents();
     },
 
-  initEvents : function () {
-    chrome.browserAction.onClicked.addListener(function(tab) {
-      chrome.tabs.captureVisibleTab(null,{
-        format: "png",
-        quality:100
-      },function(dataUri){
-      console.log(dataUri);
-      var newURL = "screenshot.html";
-      chrome.tabs.create({ url: newURL });
-      });
-    });
+    saveScreenshot : function() {
+        var image = new Image();
+        image.onload = function() {
+            var canvas = screenshot.content;
+            canvas.width = image.width;
+            canvas.height = image.height;
+            var context = canvas.getContext("2d");
+            context.drawImage(image, 0, 0);
 
-  }
-}
+            // save the image
+            var link = document.createElement('a');
+            link.download = "download.png";
+            link.href = screenshot.content.toDataURL();
+            link.click();
+            screenshot.data = '';
+        };
+        image.src = screenshot.data;
+    },
+
+    initEvents : function() {
+        chrome.browserAction.onClicked.addListener(function(tab) {
+            chrome.tabs.captureVisibleTab(null, {
+                format : "png",
+                quality : 100
+            }, function(data) {
+                screenshot.data = data;
+
+                // send an alert message to webpage
+                chrome.tabs.query({
+                    active : true,
+                    currentWindow : true
+                }, function(tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {ready : "ready"}, function(response) {
+                        if (response.download === "download") {
+                            screenshot.saveScreenshot();
+                            
+                            var newURL = "screenshot.html";
+                            chrome.tabs.create({ url: newURL });
+                        }
+                        else {
+                            screenshot.data = '';
+                        }
+                    });
+                });
+
+            });
+        });
+    }
+};
 
 screenshot.init();
--- content.js
+++ content.js
@@ -1,7 +1,8 @@
-// chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-//     if (msg.ready === "ready") {
-//         if (confirm('Do you want to have a capture of this screen?')) {
-//             sendResponse({download : "download"});
-//         }
-//     }
-// });
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+    if (msg.ready === "ready") {
+        if (confirm('Do you want to have a capture of this screen?')) {
+            sendResponse({download : "download"});
+
+        }
+    }
+});
